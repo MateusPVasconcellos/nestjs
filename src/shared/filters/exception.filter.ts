6acfc5f20b75adea151b6a115ebecd661c89570d
Vasconcellos <mateusdepaula3@gmail.com>
2023-07-16 12:05:50 -0300

    fix(app.module): Fixing Exception filter

--- src/shared/filters/exception.filter.ts
+++ src/shared/filters/exception.filter.ts
@@ -11,17 +11,18 @@ export class AllExceptionsFilter implements ExceptionFilter {
   catch(exception: unknown, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse();
-    const request = ctx.getRequest();
 
-    const status =
-      exception instanceof HttpException
-        ? exception.getStatus()
-        : HttpStatus.INTERNAL_SERVER_ERROR;
+    if (exception instanceof HttpException) {
+      return response
+        .status(exception.getStatus())
+        .json(exception.getResponse());
+    }
+
+    const status = HttpStatus.INTERNAL_SERVER_ERROR;
 
     response.status(status).json({
       statusCode: status,
-      timestamp: new Date().toISOString(),
-      path: request.url,
+      message: 'Internal Server Error',
     });
   }
 }
